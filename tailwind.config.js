f92fbe6beb4ce9a1ed4ccb2ed481cd0ef4ab15cd
hazhar24 <hazhar24ghaderi@gmail.com>
2023-07-05 17:25:30 +0430

    add costom rgba for tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'yekan-bakh': 'var(--yekan-bakh)',
         inter: 'var(--inter-google)',
       },
+      colors: {
+        'Text-color': 'rgba(51,65,95,1)',
+        'Page-background': 'rgba(237,241,244,1)',
+      },
     },
   },
   plugins: [],
