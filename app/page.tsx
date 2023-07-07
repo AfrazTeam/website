f7c82061a55d6e24c419da29be28d60b19ec14bc
Ehsan Safari <mrehsansafari@gmail.com>
2023-07-08 01:45:57 +0330

    update iconSize,topMargin,dataTypes, and added .editorconfig

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import SampleComponent from '@/components/semple-component'
 import Home from '@/app/home/page'
 
 export default function IndexApp() {
   return (
-    <div dir='rtl'>
+    <div dir="rtl">
       <Home />
     </div>
   )
--- data/header-data.ts
+++ data/header-data.ts
@@ -1,21 +1,14 @@
 interface iHeaderData {
-  name: string
-  mainPage: string
-  projects: string
-  weblog: string
-  contactUs: string
-  about: string
-  Counseling: string
+  [key: string]: string
 }
 
-let header_data: iHeaderData = {
+let header_data = {
   name: 'افراز تیم',
   mainPage: 'صفحه اصلی',
   projects: 'پروژه ها',
   weblog: 'وبلاگ',
-  contactUs: 'تماس با ما',
   about: 'درباره ما',
-  Counseling: 'مشاوره رایگان',
-}
+  freeConsultant: 'مشاوره رایگان',
+} satisfies iHeaderData
 
 export { header_data }
