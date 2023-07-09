f1d6136941784cdbf453dd2ccf217815f00d8831
Ehsan Safari <mrehsansafari@gmail.com>
2023-07-10 00:28:15 +0330

    the problem of not finding the api_data_sample file has been fixed

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { api_data_sample } from '@/data/api-sample-data'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(api_data_sample)
+  res.status(200).json({ ok: true })
 }
