75817672d7c1d7483bc1db0a8ad162348985cb68
hazhar24 <hazhar24ghaderi@gmail.com>
2023-07-09 16:26:29 +0430

    update and Sort codes

--- data/text.ts
+++ data/text.ts
@@ -1,11 +1,12 @@
 interface iTextData {
-     group:string,
-     title:string
+  group: string
+  title: string
 }
 
-let text_hero_data : iTextData ={
-     group:"گروه برنامه نویسان افراز تیم",
-     title:"ایده های خود را با خدمات کامل تیم برنامه نویسی ما به واقعیت تبدیل کنید."
+let text_hero_data: iTextData = {
+  group: 'گروه برنامه نویسان افراز تیم',
+  title:
+    'ایده های خود را با خدمات کامل تیم برنامه نویسی ما به واقعیت تبدیل کنید.',
 }
 
-export {text_hero_data}
+export { text_hero_data }
