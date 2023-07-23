5a3e6713b20eb22f793f20eccc9e78fe4b88c822
hazhar24 <hazhar24ghaderi@gmail.com>
2023-07-23 18:35:25 +0430

    Hero section changes and updates

--- data/text.ts
+++ data/text.ts
@@ -3,11 +3,14 @@ interface iDataText {
 }
 
 let dataTextGeneral: iDataText = {
-  group: 'گروه برنامه نویسان افراز تیم',
+  group: 'نیروی خلاق در زمینه ی برنامه نویسی و توسعه کسب و کار',
   title:
     'ایده های خود را با خدمات کامل تیم برنامه نویسی ما به واقعیت تبدیل کنید.',
   teammates: '"هم تیمی ها',
   projectOrder: 'سفارش پروژه',
+  counseling: 'مشاوره رایگان',
+  services: 'خدمات ما',
+  afrazteam: 'افراز تیم ',
 } satisfies iDataText
 
 export { dataTextGeneral }
