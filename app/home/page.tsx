beac4ede2bcd3b917dd785c05f1784addb8dbc4c
Ehsan Safari <mrehsansafari@gmail.com>
2023-08-10 02:17:12 +0330

    the location of the numberProjects component was changed to the hero section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,16 +2,14 @@ import { inter, yekanBakh } from '@/app/fonts'
 
 import Hero from '@/components/hero'
 import Teammates from '@/components/teammates'
-import NumberProjects from '@/components/numberProjects'
 import Services from '@/components/services'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Hero />
-      <NumberProjects />
       <Services />
       <Teammates />
-    </div>
+    </>
   )
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,41 +2,47 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { dataTextGeneral } from '@/data/text'
+import NumberProjects from './numberProjects'
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero-box">
-        <Image
-          className="animate__animated animate__flash"
-          src="svgs/Vector.svg"
-          width={80}
-          height={80}
-          alt="Vector"
-        />
-        <div>
-          <div className="">
-            <span className="hero-name">{dataTextGeneral.afrazteam}</span>
-            <span className="hero-title">{dataTextGeneral.group}</span>
+    <div className="hero-main">
+      <div className=" h-[90%] items-center flex flex-col justify-around max-h-[60rem]">
+        <div className="hero">
+          <div className="hero-box">
+            <Image
+              className="animate__animated animate__flash"
+              src="svgs/Vector.svg"
+              width={80}
+              height={80}
+              alt="Vector"
+            />
+            <div>
+              <div className="">
+                <span className="hero-name">{dataTextGeneral.afrazteam}</span>
+                <span className="hero-title">{dataTextGeneral.group}</span>
+              </div>
+            </div>
+            <p className="hero-descrption">{dataTextGeneral.title}</p>
+            <div className="hero-button">
+              <button className="hero-counseling">
+                <Link href="/">{dataTextGeneral.counseling}</Link>
+              </button>
+              <button className="hero-services">
+                <Link href="/">{dataTextGeneral.ourservices}</Link>
+              </button>
+            </div>
+          </div>
+          <div className="hero-svg animate__animated animate__zoomIn">
+            <Image
+              src="/svgs/team1.svg"
+              width={500}
+              height={500}
+              alt="afraz team"
+            />
           </div>
         </div>
-        <p className="hero-descrption">{dataTextGeneral.title}</p>
-        <div className="hero-button">
-          <button className="hero-counseling">
-            <Link href="/">{dataTextGeneral.counseling}</Link>
-          </button>
-          <button className="hero-services">
-            <Link href="/">{dataTextGeneral.ourservices}</Link>
-          </button>
-        </div>
-      </div>
-      <div className="hero-svg animate__animated animate__zoomIn">
-        <Image
-          src="/svgs/team1.svg"
-          width={500}
-          height={500}
-          alt="afraz team"
-        />
+        <NumberProjects />
       </div>
     </div>
   )
