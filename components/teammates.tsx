897cbaffbe28cd86368c04cc03b4d57d0bd182da
Ehsan Safari <mrehsansafari@gmail.com>
2023-08-10 07:52:23 +0330

    deleted teammates component and some photo

--- components/teammates.tsx
+++ components/teammates.tsx
@@ -7,30 +7,6 @@ const Teammates = () => {
   return (
     <div>
       <p className="teammates-p">{dataTextGeneral.teammates}</p>
-      <div className="teammates-main">
-        {dataTeammates.map((item) => (
-          <div key={item.id} className="teammates-svg">
-            <Image
-              src="/svgs/teammates.svg"
-              height={750}
-              width={380}
-              alt="hhh"
-            />
-            <div className="teammates-image-user">
-              <Image
-                className="teammates-image"
-                style={{ width: '80px', objectFit: 'cover', height: '80px' }}
-                src={item.image}
-                width={90}
-                height={90}
-                alt="imageuser"
-              />
-            </div>
-            <span className="teammates-name">{item.name}</span>
-            <span className="teammates-skill">{item.skills}</span>
-          </div>
-        ))}
-      </div>
     </div>
   )
 }
