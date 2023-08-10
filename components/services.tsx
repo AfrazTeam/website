826b20225d18044f7e65fb07b7c80d7ba7841215
Ehsan Safari <mrehsansafari@gmail.com>
2023-08-10 07:38:34 +0330

    update team photo

--- components/services.tsx
+++ components/services.tsx
@@ -4,61 +4,79 @@ import Image from 'next/image'
 
 const Services = () => {
   return (
-    <div className='services'>
-      <div className='services-top'>
-        <div className='services-top-item'>
-          <div className='services-text'>
-            <Image src='svgs/hope.svg' width={30} height={30} alt='hope' />
-            <p className='pr-3'>{dataTextGeneral.services}</p>
+    <div className="services">
+      <div className="services-top">
+        <div className="services-top-item">
+          <div className="services-text">
+            <Image src="svgs/Hope.svg" width={30} height={30} alt="hope" />
+            <p className="pr-3">{dataTextGeneral.services}</p>
           </div>
-          <p className='services-title'>{dataTextGeneral.titleServices}</p>
+          <p className="services-title">{dataTextGeneral.titleServices}</p>
         </div>
-        <div className='services-top-item flex justify-end'>
-          <Image src='svgs/team2.svg' width={500} height={100} alt='ss' />
+        <div className="services-top-item flex justify-end">
+          <Image src="/images/team2.png" width={500} height={100} alt="ss" />
         </div>
       </div>
-      <div className='services-bottom'>
-        <div className='services-bottom-item'>
-          <div className='services-bottom-name'>
-            <Image src='svgs/svg6.svg' width={40} height={30} alt='svg1' />
-            <span className='services-span'>{dataTextGeneral.appMobile}</span>
+      <div className="services-bottom">
+        <div className="services-bottom-item">
+          <div className="services-bottom-name">
+            <Image src="svgs/svg6.svg" width={40} height={30} alt="svg1" />
+            <span className="services-span">{dataTextGeneral.appMobile}</span>
           </div>
-          <p className='services-bottom-title'>{dataTextGeneral.appMobileTitle}</p>
+          <p className="services-bottom-title">
+            {dataTextGeneral.appMobileTitle}
+          </p>
         </div>
-        <div className='services-bottom-item'>
-          <div className='services-bottom-name'>
-            <Image src='svgs/svg2.svg' width={40} height={30} alt='svg1' />
-            <span className='services-span'>{dataTextGeneral.telegramBot}</span>
+        <div className="services-bottom-item">
+          <div className="services-bottom-name">
+            <Image src="svgs/svg2.svg" width={40} height={30} alt="svg1" />
+            <span className="services-span">{dataTextGeneral.telegramBot}</span>
           </div>
-          <p className='services-bottom-title'>{dataTextGeneral.telegranBotTitle}</p>
+          <p className="services-bottom-title">
+            {dataTextGeneral.telegranBotTitle}
+          </p>
         </div>
-        <div className='services-bottom-item'>
-          <div className='services-bottom-name'>
-            <Image src='svgs/svg1.svg' width={40} height={30} alt='svg1' />
-            <span className='services-span'>{dataTextGeneral.webDesign}</span>
+        <div className="services-bottom-item">
+          <div className="services-bottom-name">
+            <Image src="svgs/svg1.svg" width={40} height={30} alt="svg1" />
+            <span className="services-span">{dataTextGeneral.webDesign}</span>
           </div>
-          <p className='services-bottom-title'>{dataTextGeneral.webDesignTitle}</p>
+          <p className="services-bottom-title">
+            {dataTextGeneral.webDesignTitle}
+          </p>
         </div>
-        <div className='services-bottom-item'>
-          <div className='services-bottom-name'>
-            <Image src='svgs/svg3.svg' width={40} height={30} alt='svg1' />
-            <span className='services-span'>{dataTextGeneral.teachingProgramming}</span>
+        <div className="services-bottom-item">
+          <div className="services-bottom-name">
+            <Image src="svgs/svg3.svg" width={40} height={30} alt="svg1" />
+            <span className="services-span">
+              {dataTextGeneral.teachingProgramming}
+            </span>
           </div>
-          <p className='services-bottom-title'>{dataTextGeneral.teachingProgrammingTitle}</p>
+          <p className="services-bottom-title">
+            {dataTextGeneral.teachingProgrammingTitle}
+          </p>
         </div>
-        <div className='services-bottom-item'>
-          <div className='services-bottom-name'>
-            <Image src='svgs/svg4.svg' width={40} height={30} alt='svg1' />
-            <span className='services-span'>{dataTextGeneral.businessDevelopment}</span>
+        <div className="services-bottom-item">
+          <div className="services-bottom-name">
+            <Image src="svgs/svg4.svg" width={40} height={30} alt="svg1" />
+            <span className="services-span">
+              {dataTextGeneral.businessDevelopment}
+            </span>
           </div>
-          <p className='services-bottom-title'>{dataTextGeneral.businessDevelopmentTitle}</p>
+          <p className="services-bottom-title">
+            {dataTextGeneral.businessDevelopmentTitle}
+          </p>
         </div>
-        <div className='services-bottom-item'>
-          <div className='services-bottom-name'>
-            <Image src='svgs/svg5.svg' width={40} height={30} alt='svg1' />
-            <span className='services-span'>{dataTextGeneral.professionalSupport}</span>
+        <div className="services-bottom-item">
+          <div className="services-bottom-name">
+            <Image src="svgs/svg5.svg" width={40} height={30} alt="svg1" />
+            <span className="services-span">
+              {dataTextGeneral.professionalSupport}
+            </span>
           </div>
-          <p className='services-bottom-title'>{dataTextGeneral.professionalSupportTitle}</p>
+          <p className="services-bottom-title">
+            {dataTextGeneral.professionalSupportTitle}
+          </p>
         </div>
       </div>
     </div>
