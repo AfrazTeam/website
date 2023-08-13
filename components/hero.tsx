255d4135a6d5eb78fe134ae5f328a898d1640296
Ehsan Safari <mrehsansafari@gmail.com>
2023-08-13 04:18:34 +0330

    add a description above the title hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,16 +10,23 @@ const Hero = () => {
       <div className=" h-[90%] items-center flex flex-col justify-around max-h-[60rem]">
         <div className="hero">
           <div className="hero-box">
-            <Image
+            <div className="flex items-center pt-10 ">
+              <Image
               className="animate__animated animate__flash"
-              src="svgs/Vector.svg"
-              width={80}
-              height={80}
-              alt="Vector"
-            />
-            <div>
-              <span className="hero-title">{dataTextGeneral.group}</span>
+                src="svgs/Vector.svg"
+                width={30}
+                height={30}
+                alt="Vector"
+              />
+              <span
+                className="text-sm mx-2 bg-gradient-to-tr from-cyan-600 to-cyan-700 rounded-md 
+               px-2 py-1 text-transparent bg-clip-text"
+              >
+                با ما کسب و کار خود را رشد دهید
+              </span>
             </div>
+            <span className="hero-title">{dataTextGeneral.group}</span>
+
             <p className="hero-descrption animate__animated animate__flash">
               {dataTextGeneral.title}
             </p>
@@ -34,6 +41,7 @@ const Hero = () => {
           </div>
           <div className="hero-svg animate__animated animate__zoomIn">
             <Image
+              className="rotate-scale-up"
               src="/svgs/team1.svg"
               width={500}
               height={500}
