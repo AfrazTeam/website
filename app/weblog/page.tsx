c80f532e4d1563c1589af1cba120528c1011e5d8
hazhar24 <hazhar24ghaderi@gmail.com>
2024-06-12 18:36:19 +0330

    add file weblog for header

---
+++ app/weblog/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Blog() {
+	return (
+		<div>
+			<p className="container mx-auto mt-16 flex w-full flex-col items-center justify-center px-5">
+				صفحه weblog در حال ساخت
+			</p>
+		</div>
+	);
+}
