93bf1e31e05fff4a016e4067a44def09e6cb7d0f
hazhar24 <hazhar24ghaderi@gmail.com>
2024-06-12 18:35:29 +0330

    add file projects for header

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Projects() {
+	return (
+		<div className="container mx-auto mt-16 flex w-full flex-col items-center justify-center px-5">
+			<p>صفحه projects در حال ساخت</p>
+		</div>
+	);
+}
