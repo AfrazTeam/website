0285c0d8abf40f692648fc1a206f7abbdc346b81
hazhar24 <hazhar24ghaderi@gmail.com>
2024-06-12 18:34:53 +0330

    add file about for header

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+	return (
+		<div>
+			<p className="container mx-auto mt-16 flex w-full flex-col items-center justify-center px-5">
+				صفحه about در حال ساخت
+			</p>
+		</div>
+	);
+}
